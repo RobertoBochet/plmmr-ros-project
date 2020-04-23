96df5eab103dd21aa682527afe8b4d6cdd218a18
Roberto Bochet <robertobochet@gmail.com>
2020-04-23 06:01:25 +0200

    Add status check function

--- src/status_check/src/status_check.cpp
+++ src/status_check/src/status_check.cpp
@@ -3,6 +3,11 @@
 #include <dynamic_reconfigure/server.h>
 #include <status_check/parametersConfig.h>
 
+#include <std_msgs/Float64.h>
+#include <std_msgs/String.h>
+
+#include <math.h>
+
 
 class StatusCheckNode
 {
@@ -11,13 +16,16 @@ class StatusCheckNode
 	ros::NodeHandle nh;
 	dynamic_reconfigure::Server<status_check::parametersConfig> config_server;
 
+	ros::Publisher status_pub;
+	ros::Subscriber dist_sub;
+
 	void config_callback(status_check::parametersConfig &config, uint32_t level)
 	{
 		if (level == 0)
 		{
 			param.crash_limit = config.crash_limit;
 			param.safe_limit = (param.crash_limit <= config.safe_limit) ? config.safe_limit : param.crash_limit;
-		} else if (level == 1)
+		} else
 		{
 			param.safe_limit = config.safe_limit;
 			param.crash_limit = (param.safe_limit >= config.crash_limit) ? config.crash_limit : param.safe_limit;
@@ -31,6 +39,27 @@ class StatusCheckNode
 		ROS_INFO("Reconfigure parameters: {crash_limit: %f, safe_limit: %f}", param.crash_limit, param.safe_limit);
 	}
 
+	void dist_callback(const std_msgs::Float64::ConstPtr &dist)
+	{
+		// ignores message if it is NaN
+		if (isnan(dist->data)) return;
+
+		std_msgs::String msg;
+
+		if (dist->data < param.crash_limit)
+			msg.data = "crash";
+
+		else if (dist->data < param.safe_limit)
+			msg.data = "unsafe";
+
+		else
+			msg.data = "safe";
+
+		status_pub.publish(msg);
+
+		ROS_INFO("Status is %s", msg.data.c_str());
+	}
+
 public:
 	StatusCheckNode()
 	{
@@ -42,6 +71,12 @@ public:
 		config_server.setConfigDefault(param);
 
 		config_server.setCallback([this](auto &&PH1, auto &&PH2) { return config_callback(PH1, PH2); });
+
+		status_pub = nh.advertise<std_msgs::String>("status", 10);
+
+		dist_sub = nh.subscribe("dist", 1, &StatusCheckNode::dist_callback, this);
+
+		ROS_INFO("Status check node ready!");
 	}
 };
 
