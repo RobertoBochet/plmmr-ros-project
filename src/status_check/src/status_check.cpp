44dfd5c6e729a3deeae4b05db2f673c842f09a3d
Roberto Bochet <robertobochet@gmail.com>
2020-04-25 17:32:30 +0200

    Add odometrical handler

--- src/status_check/src/status_check.cpp
+++ src/status_check/src/status_check.cpp
@@ -3,21 +3,43 @@
 #include <dynamic_reconfigure/server.h>
 #include <status_check/parametersConfig.h>
 
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <nav_msgs/Odometry.h>
+
+#include <distance_service/DistanceCalculator.h>
+
+#include <status_check/Status.h>
+
 #include <std_msgs/Float64.h>
 #include <std_msgs/String.h>
 
-#include <math.h>
+#include <cmath>
 
 
 class StatusCheckNode
 {
+public:
+	typedef message_filters::sync_policies::ApproximateTime<nav_msgs::Odometry, nav_msgs::Odometry> SyncApproxPolicy;
+	typedef message_filters::Synchronizer<SyncApproxPolicy> Sync;
+
 	status_check::parametersConfig param;
 
 	ros::NodeHandle nh;
+
 	dynamic_reconfigure::Server<status_check::parametersConfig> config_server;
 
 	ros::Publisher status_pub;
-	ros::Subscriber dist_sub;
+
+	message_filters::Subscriber<nav_msgs::Odometry> sub_odom_front;
+	message_filters::Subscriber<nav_msgs::Odometry> sub_odom_obs;
+
+	std::shared_ptr<Sync> sync_ptr;
+
+	ros::ServiceClient distance_calculator;
+
 
 	void config_callback(status_check::parametersConfig &config, uint32_t level)
 	{
@@ -39,45 +61,76 @@ class StatusCheckNode
 		ROS_INFO("Reconfigure parameters: {crash_limit: %f, safe_limit: %f}", param.crash_limit, param.safe_limit);
 	}
 
-	void dist_callback(const std_msgs::Float64::ConstPtr &dist)
+	void callback(const nav_msgs::Odometry::ConstPtr &front, const nav_msgs::Odometry::ConstPtr &obs)
 	{
-		// ignores message if it is NaN
-		if (isnan(dist->data)) return;
+		status_check::Status s;
 
-		std_msgs::String msg;
+		// handles the GPS signal loss
+		if (front->header.frame_id == "front_lost" || obs->header.frame_id == "obs_lost")
+		{
+			s.distance = NAN;
+			s.status = "unavailable";
 
-		if (dist->data < param.crash_limit)
-			msg.data = "crash";
+			status_pub.publish(s);
 
-		else if (dist->data < param.safe_limit)
-			msg.data = "unsafe";
+			ROS_INFO("The distance is unavailable");
+			return;
+		}
+
+		// prepares the odometrical data for the distance service
+		distance_service::DistanceCalculator srv;
+		srv.request.a = front->pose.pose.position;
+		srv.request.b = obs->pose.pose.position;
+
+		// checks if the service worked
+		if (!distance_calculator.call(srv))
+		{
+			ROS_ERROR("The distance service failed");
+			return;
+		}
+
+		// publishes status topic's data
+		s.distance = srv.response.dist;
+
+		if (s.distance < param.crash_limit)
+			s.status = "crash";
+
+		else if (s.distance < param.safe_limit)
+			s.status = "unsafe";
 
 		else
-			msg.data = "safe";
+			s.status = "safe";
 
-		status_pub.publish(msg);
+		status_pub.publish(s);
 
-		ROS_INFO("Status is %s", msg.data.c_str());
+		ROS_INFO("The distance is %fm and the status is %s", s.distance, s.status.c_str());
+	}
+
+	inline void sub_setup()
+	{
+		sub_odom_front.subscribe(nh, "odom/front", 1);
+		sub_odom_obs.subscribe(nh, "odom/obs", 1);
+
+		sync_ptr = std::make_shared<Sync>(SyncApproxPolicy(10), sub_odom_front, sub_odom_obs);
+
+		sync_ptr->registerCallback(&StatusCheckNode::callback, this);
 	}
 
 public:
+
 	StatusCheckNode()
 	{
-		ros::NodeHandle nhl("~");
-
-		nhl.getParam("safe_limit", param.safe_limit);
-		nhl.getParam("crash_limit", param.crash_limit);
-
-		config_server.setConfigDefault(param);
-
 		config_server.setCallback([this](auto &&PH1, auto &&PH2) { return config_callback(PH1, PH2); });
 
-		status_pub = nh.advertise<std_msgs::String>("status", 10);
+		distance_calculator = nh.serviceClient<distance_service::DistanceCalculator>("distance_calculator");
 
-		dist_sub = nh.subscribe("dist", 1, &StatusCheckNode::dist_callback, this);
+		status_pub = nh.advertise<status_check::Status>("status", 10);
+
+		sub_setup();
 
 		ROS_INFO("Status check node ready!");
 	}
+
 };
 
 int main(int argc, char **argv)
@@ -85,6 +138,11 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "status_check");
 
 	StatusCheckNode node;
+
+	//node.sub_odom_front.subscribe(node.nh, "odom/front", 1);
+	//node.sub_odom_obs.subscribe(node.nh, "odom/obs", 1);
+
+	//node.sync.reset(new StatusCheckNode::Sync(StatusCheckNode::MySyncPolicy(10), node.sub_odom_front, node.sub_odom_obs));
 
 	ros::spin();
 	return 0;
